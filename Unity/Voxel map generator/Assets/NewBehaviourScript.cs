b4ceb74c21e84a4e223f1cc2daf2d23527dcc72d
Tom smith <fudgeman3000@gmail.com>
2021-01-12 02:52:30 +0000

    First commit

---
+++ Unity/Voxel map generator/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	//init chunk size
+	public int x_blocks = 50;
+	public int z_blocks = 50;
+
+
+	//perlin variables
+	public float amp = 3.0f;
+	public float freq = 6.0f;
+	public float seed = 123;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
